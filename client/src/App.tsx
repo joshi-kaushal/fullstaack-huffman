8c2ec62875b8711855b104d4d64aa591958beaf0
Kaushal Joshi <7joshikaushal@gmail.com>
2023-06-22 15:46:10 +0530

    refractor: buttons and basic fetch logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,28 @@
 import "./App.css";
 
 function App() {
+  const fetchData = async (type: string) => {
+    try {
+      const data = await fetch(`http://localhost:5000/fetch?type=${type}`).then(
+        (response) => response.json()
+      );
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <>
       <h1>Huffman Implementation</h1>
       <div className="buttons">
-        <button>Fetch with encode</button>
+        <button onClick={() => fetchData("withEncode")}>
+          Fetch with encode
+        </button>
 
-        <button>Fetch without encode</button>
+        <button onClick={() => fetchData("withoutEncode")}>
+          Fetch without encode
+        </button>
       </div>
     </>
   );
