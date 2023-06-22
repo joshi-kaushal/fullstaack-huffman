309270831863a07606ab3fe761461b5c82c387f5
Kaushal Joshi <7joshikaushal@gmail.com>
2023-06-22 15:47:06 +0530

    refractor: cors, fetch w/o encode

--- server/server.js
+++ server/server.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+app.use(cors());
+
 const PORT = 5000;
 
+const data = require("./data.json");
+
 app.get("/", (req, res) => {
-  res.status(200).send("Hello World!");
+  res.status(200).send("Response from server: Everything is working OK!");
+});
+
+app.get("/fetch", async (req, res) => {
+  const type = req.query.type;
+
+  if (type === "withoutEncode") {
+    // const data = fetchWithoutEncode();
+    res.status(200).json({ message: "Status ok", data: data });
+  }
 });
 
 app.listen(PORT, () => {
