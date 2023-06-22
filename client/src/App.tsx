87c6c188da269e0b754f9dfc8e8c1363b13afbe6
Kaushal Joshi <7joshikaushal@gmail.com>
2023-06-22 15:07:19 +0530

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Huffman Implementation</h1>
+      <div className="buttons">
+        <button>Fetch with encode</button>
+
+        <button>Fetch without encode</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
