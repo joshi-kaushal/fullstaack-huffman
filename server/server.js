fce12ecd5b0eb47fec8fed41afd6fe17eff1a509
Kaushal Joshi <7joshikaushal@gmail.com>
2023-06-23 16:23:24 +0530

    feat: huffman algo implemented

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 
+const { fetchWithEncode, fetchWithoutEncode } = require("./utils/utils");
+
 const app = express();
 app.use(cors());
+// app.use(fetchWithoutEncode());
 
 const PORT = 5000;
-
-const data = require("./data.json");
 
 app.get("/", (req, res) => {
   res.status(200).send("Response from server: Everything is working OK!");
@@ -14,10 +15,12 @@ app.get("/", (req, res) => {
 
 app.get("/fetch", async (req, res) => {
   const type = req.query.type;
-
   if (type === "withoutEncode") {
-    // const data = fetchWithoutEncode();
+    const data = fetchWithoutEncode();
     res.status(200).json({ message: "Status ok", data: data });
+  } else {
+    const data = fetchWithEncode();
+    res.status(200).json({ data });
   }
 });
 
